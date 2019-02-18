85af7f9ba35d98c0c1d27c05a0d305d4cd2d16ae
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-02-18 16:45:02 -0300

    Update createCDDiagram.R

--- CDDiagrams/createCDDiagram.R
+++ CDDiagrams/createCDDiagram.R
@@ -1,5 +1,8 @@
 library("scmamp")
 library("ggplot2")
 library("Rgraphviz")
-dados = read.csv("AUPRC.csv", header = TRUE, sep = ",")
-plotCD (dados, alpha=0.05, cex=1.25, decreasing = FALSE)
+measures = c("AUPRC","Weighted","Pooled")
+for(i in 1:length(measures)){
+	dados = read.csv(paste(measures[i],"csv",sep="."), header = TRUE, sep = ",")
+	plotCD (dados, alpha=0.05, cex=1.25, decreasing = FALSE)
+}
