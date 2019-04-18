ba014635a9c1a13027dea4df88b0e16c9387248b
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-04-18 09:10:23 -0300

    Correcao

--- CDDiagrams/createCDDiagram.R
+++ CDDiagrams/createCDDiagram.R
@@ -4,5 +4,5 @@ library("Rgraphviz")
 measures = c("AUPRC","Weighted","Pooled")
 for(i in 1:length(measures)){
 	dados = read.csv(paste(measures[i],"csv",sep="."), header = TRUE, sep = ",")
-	plotCD (dados, alpha=0.05, cex=1.25, decreasing = FALSE)
+	plotCD (dados, alpha=0.05, cex=1.25, decreasing = TRUE)
 }
