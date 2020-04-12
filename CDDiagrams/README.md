5b764d144dcd3b59042f89c2e99d8446fddf98f1
bzamith <bruna.zamith@hotmail.com>
2020-04-12 16:24:04 -0300

    Removed old diagrams

--- CDDiagrams/README.md
+++
@@ -1,2 +0,0 @@
-Da [Tabela de resultados](/PBCT%20Results%20Feb2019.xlsx), eu copiei cada coluna de cada medida para cada dataset. Colei "de qualquer jeito" nos arquivos txt. Rodei o python para gerar os csv que servem de entrada para o R, cuja função é gerar os diagramas de CD.
-
--- CDDiagrams/createCDDiagram.R
+++
@@ -1,8 +0,0 @@
-library("scmamp")
-library("ggplot2")
-library("Rgraphviz")
-measures = c("AUPRC","Weighted","Pooled")
-for(i in 1:length(measures)){
-	dados = read.csv(paste(measures[i],"csv",sep="."), header = TRUE, sep = ",")
-	plotCD (dados, alpha=0.05, cex=1.25, decreasing = TRUE)
-}
